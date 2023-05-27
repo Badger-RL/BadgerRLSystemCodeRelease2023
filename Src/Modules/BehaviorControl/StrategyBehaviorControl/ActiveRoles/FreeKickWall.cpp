94f26370c64e614a915f76f8f4765c6cb51658b7
byyhong <wzry24680@gmail.com>
2023-05-27 13:02:44 -0500

    Modified FreeKickWall.cpp and Behavior.cpp to implement kick-in

--- Src/Modules/BehaviorControl/StrategyBehaviorControl/ActiveRoles/FreeKickWall.cpp
+++ Src/Modules/BehaviorControl/StrategyBehaviorControl/ActiveRoles/FreeKickWall.cpp
@@ -8,20 +8,49 @@
  */
 
 #include "FreeKickWall.h"
-
+#include <iostream>
 SkillRequest FreeKickWall::execute(const Agent&, const Agents&)
 {
-  const Vector2f ballPosition = theFieldBall.recentBallPositionOnField(); // the position of the ball on the field
-  const Vector2f defenderVector = Vector2f(theFieldDimensions.xPosOwnPenaltyMark, 0.f) - ballPosition; // change of direction
-  const Vector2f newTarget = ballPosition + defenderVector.normalized(normalizeValue); // calculated new target
-  const Angle rotationAngle = defenderVector.angle(); // angle for the rotation of the direction
-  const Vector2f rotatedNewTargetPosition = newTarget.rotated(-rotationAngle); // rotated new target
-  const Vector2f rotatedOldTargetPosition = currentTarget.rotated(-rotationAngle); // rotated old target
+  const Vector2f ballPosition = theFieldBall.recentBallPositionOnField();
+  
+//  std::cout << defenderVector.normalized(normalizeValue) << std::endl;
+  const Vector2f shift =  (-1 * ballPosition).normalized(normalizeValue);
+  const Vector2f targetPosition = ballPosition + shift;
+//  const Vector2f defenderVector = Vector2f(theFieldDimensions.xPosOwnPenaltyMark, 0.f) - ballPosition; // change of direction
+//  const Vector2f newTarget = ballPosition + defenderVector.normalized(normalizeValue); // calculated new target
+//  const Angle rotationAngle = defenderVector.angle(); // angle for the rotation of the direction
+//  const Vector2f rotatedNewTargetPosition = newTarget.rotated(-rotationAngle); // rotated new target
+//  const Vector2f rotatedOldTargetPosition = currentTarget.rotated(-rotationAngle); // rotated old target
+//    std::cout << "Ball Position x: " << ballPosition.x() << std::endl;
+//    std::cout << "Ball Position y: " << ballPosition.y() << std::endl;
+//    std::cout << "shift x: " << shift.x() << std::endl;
+//    std::cout << "shift y: " << shift.y() << std::endl;
+//        std::cout << "Target Target x: " << targetPosition.x() << std::endl;
+//        std::cout << "Target Target y: " << targetPosition.y() << std::endl;
+//    std::cout << "Defender Vector y: " << defenderVector.y() << std::endl;
+//    std::cout << "New Target x: " << newTarget.x() << std::endl;
+//    std::cout << "New Target y: " << newTarget.y() << std::endl;
+//    std::cout << "Current Target x: " << currentTarget.x() << std::endl;
+//    std::cout << "Current Target y: " << currentTarget.y() << std::endl;
+//    std::cout << "Angle: " << rotationAngle.toDegrees() << std::endl;
+//    std::cout << "rotated New Target Position x: " << rotatedNewTargetPosition.x() << std::endl;
+//    std::cout << "rotated New Target Position y: " << rotatedNewTargetPosition.y() << std::endl;
+//    std::cout << "rotated Old Target Position x: " << rotatedOldTargetPosition.x() << std::endl;
+//    std::cout << "rotated Old Target Position y: " << rotatedOldTargetPosition.y() << std::endl;
+//    std::cout << "Ball Position - current Target angle: " << (ballPosition - currentTarget).angle() << std::endl;
+
   /**
    * if the new target is within the threshold keep the position otherwise walk to the new target.
    */
-  if(std::abs(rotatedNewTargetPosition.x() - rotatedOldTargetPosition.x()) > maxXValue ||
-     (std::abs(rotatedNewTargetPosition.y() - rotatedOldTargetPosition.y()) > maxYValue))
-    currentTarget = newTarget;
+  if(std::abs(targetPosition.x() - ballPosition.x()) > maxXValue ||
+     (std::abs(targetPosition.y() - ballPosition.y()) > maxYValue))
+    currentTarget = targetPosition;
+//    return SkillRequest::Builder::walkTo(Pose2f(-1000.f, 0.f));
+    
+  
+  
   return SkillRequest::Builder::walkTo(Pose2f((ballPosition - currentTarget).angle(), currentTarget));
+    
+    
+    
 }
