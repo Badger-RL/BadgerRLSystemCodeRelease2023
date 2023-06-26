50b4ad1bc7256ccef9f9b1eb5919e59079f43080
byyhong <wzry24680@gmail.com>
2023-06-26 15:00:47 -0500

    Modified Neural Control

--- Src/Modules/BehaviorControl/SkillBehaviorControl/Skills/Walk/NeuralControl.cpp
+++ Src/Modules/BehaviorControl/SkillBehaviorControl/Skills/Walk/NeuralControl.cpp
@@ -69,7 +69,7 @@ json::object timeData = json::object{};
 json::object prevObservationData = json::object{};
 std::vector<int> robotNum;
 
-bool isSimRobot = true;
+bool isSimRobot = false;
 bool robotPreCollision = false;
 
 json::object preRole = json::object{}; // This is used to record the role assigned by role assignment method.
